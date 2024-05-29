6201fa70cb2aa147a28df69781caebd225a40ebe
Arnav Borborah <arnavborborah11@gmail.com>
2024-05-29 15:50:28 -0400

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿open Discord
+
+let testCommandBuilder =
+    (new SlashCommandBuilder())
+        .WithName("list-roles")
+        .WithDescription("Lists all roles of a user.")
+        .AddOption(
+            "user",
+            ApplicationCommandOptionType.User,
+            "The users whos roles you want to be listed",
+            isRequired = true
+        )
